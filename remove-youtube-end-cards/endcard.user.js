e4e2703f2481caa075dc4ee7ac7e072ae2e326ac
WalkCat <flamemevid@gmail.com>
2018-10-23 21:28:53 +0300

    Initial commit

---
+++ remove-youtube-end-cards/endcard.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @author       Dim
+// @description  Removes the end cards of YouTube videos
+// @name         Remove YouTube End Cards
+// @namespace    https://dim.codes
+// @version      1.3.0
+// @icon         https://www.youtube.com/yts/img/favicon_96-vfldSA3ca.png
+// @match        https://www.youtube.com/*
+// @updateURL    https://dim.codes/remove-youtube-end-cards/endcard.user.js
+// @run-at       document-start
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// ==/UserScript==
+
+function disableSPF() {
+  if (unsafeWindow._spf_state && unsafeWindow._spf_state.config) {
+    unsafeWindow._spf_state.config['navigate-limit'] = 0;
+    unsafeWindow._spf_state.config['navigate-part-received-callback'] = function (targetUrl) { location.href = targetUrl; };
+  }
+  setTimeout(disableSPF, 50);
+}
+
+function installUnsafewindowPolyfill() {
+  if (typeof unsafeWindow === 'undefined') {
+    if (typeof XPCNativeWrapper === 'function' && typeof XPCNativeWrapper.unwrap === 'function') unsafeWindow = XPCNativeWrapper.unwrap(window);
+    else if (window.wrappedJSObject) unsafeWindow = window.wrappedJSObject;
+  }
+}
+
+function removeCard() {
+  setTimeout(function() {
+    if ((document.getElementsByTagName('ytd-subscribe-button-renderer').length ||
+         document.querySelector('ytd-button-renderer.ytd-video-secondary-info-renderer') ||
+         document.getElementById('watch7-subscription-container')) &&
+         !document.getElementById('remove_card')) {
+      var endCardRemover = document.createElement('span');
+      endCardRemover.appendChild(document.createTextNode('Remove End Cards'));
+      endCardRemover.id = 'remove_card';
+      var sub = document.getElementsByTagName('ytd-subscribe-button-renderer')[0] ? document.getElementsByTagName('ytd-subscribe-button-renderer')[0] : document.querySelector('ytd-button-renderer.ytd-video-secondary-info-renderer');
+      if (sub) {
+        GM_addStyle('#remove_card{user-select:none;margin:auto 4px;font-weight:500;text-transform:uppercase;letter-spacing:.007px;;background-color:hsl(0, 0%, 93.3%);color:hsla(0, 0%, 6.7%, .6);cursor:pointer;padding:10px 16px;border-radius:2px;vertical-align:middle;font-size:1.4rem}');
+        sub.insertBefore(endCardRemover, sub.firstChild);
+      } else {
+        GM_addStyle('#remove_card{user-select:none;border:1px solid #CCC;background-color:#FAFAFA;color:#737373;cursor:pointer;padding:4px 8px;border-radius:2px;margin-left:10px;vertical-align:middle;font-size:12px}');
+        document.getElementById('watch7-subscription-container').appendChild(endCardRemover);
+      }
+      endCardRemover.onclick = function() {
+        var cards = document.getElementsByClassName('ytp-ce-element');
+        while(cards[0]) {
+          cards[0].parentNode.removeChild(cards[0]);
+        }
+        endCardRemover.innerText = 'End Cards Removed';
+      };
+    }
+    removeCard();
+  }, 100);
+}
+
+disableSPF();
+installUnsafewindowPolyfill();
+if (location.pathname === '/watch') document.addEventListener('DOMContentLoaded', removeCard);
---
+++ src/js/global.js
@@ -0,0 +1,172 @@
+const center = document.querySelector('.center');
+const cvs = document.querySelector('canvas');
+const ctx = cvs.getContext('2d');
+const pog = new Image();
+pog.src = 'https://cdn.discordapp.com/emojis/440658615880253440.png';
+
+const height = 80;
+const size = 20;
+
+let leftWins = 0;
+let rightWins = 0;
+let poggers = false;
+
+const reset = () => {
+	cvs.width = innerWidth;
+	cvs.height = innerHeight;
+
+	ctx.fillRect(0, 0, cvs.width, cvs.height);
+	ctx.fillStyle = '#FFFFFF';
+	ctx.fillRect(0, 0, cvs.width, size);
+	ctx.fillRect(0, cvs.height - size, cvs.width, size);
+	ctx.font = '40px monospace';
+	ctx.textAlign = 'right';
+	ctx.fillText(leftWins, (cvs.width / 2) - 20, size + 50);
+	ctx.textAlign = 'left';
+	ctx.fillText(rightWins, (cvs.width / 2) + 20, size + 50);
+
+	for (let i = 0; i < cvs.width; i += size)
+		ctx.fillRect((cvs.width / 2) - (size / 2), (i * 2) - (size / 2), size, size);
+};
+
+class Base {
+	get left() {
+		return this.x;
+	}
+
+	get right() {
+		return this.x + this.width;
+	}
+
+	get top() {
+		return this.y;
+	}
+
+	get bot() {
+		return this.y + this.height;
+	}
+}
+
+class Paddle extends Base {
+	constructor(x) {
+		super();
+		this.width = 20;
+		this.height = 80;
+
+		this.x = x;
+		this.calcY();
+	}
+
+	calcY() {
+		this.y = (cvs.height - this.height) / 2;
+	}
+
+	move(y) {
+		this.moveTo(this.y - y);
+	}
+
+	moveTo(y) {
+		this.y = Math.max(Math.min(y, cvs.height - size - this.height), 0 + size);
+	}
+}
+
+class Ball extends Base {
+	constructor() {
+		super();
+		this.x = cvs.width / 2;
+		this.y = cvs.height / 2;
+		this.speed = 7;
+		this.xDelta = Math.round(Math.random()) ? this.speed : -this.speed;
+		this.yDelta = 0;
+
+		this.width = 15;
+		this.height = 15;
+	}
+}
+
+const play = () => {
+	let ended = false;
+	const leftPaddle = new Paddle(0);
+	const rightPaddle = new Paddle(cvs.width - size);
+	const ball = new Ball();
+
+	const keys = {
+		ArrowUp: () => rightPaddle.move(2),
+		ArrowDown: () => rightPaddle.move(-2),
+	};
+	let sequence = '';
+
+	window.onkeydown = evt => {
+		sequence += evt.key.toLowerCase();
+		if (sequence.endsWith('poggers')) poggers = !poggers;
+		const action = keys[evt.key];
+		if (action) {
+			if (action.pressed) return;
+			action.pressed = true;
+			const interval = setInterval(() => {
+				if (!action.pressed) return clearInterval(interval);
+				action();
+			}, 3);
+		}
+	};
+
+	window.onkeyup = evt => {
+		const action = keys[evt.key];
+		if (action) action.pressed = false;
+	};
+
+	const scoreInterval = setInterval(() => {
+		if (ended) return clearInterval(scoreInterval);
+
+		ball.x += ball.xDelta;
+		ball.y += ball.yDelta;
+
+		if (ball.top <= leftPaddle.top) leftPaddle.move(2.5);
+		if (ball.bot >= leftPaddle.bot) leftPaddle.move(-2.5);
+
+		if (ball.top <= size || ball.bot >= cvs.height - size) ball.yDelta *= -1;
+		const touchingLeft = ball.left <= leftPaddle.right && ball.top <= leftPaddle.bot && ball.bot >= leftPaddle.top && ball.xDelta === -ball.speed;
+		const touchingRight = ball.right >= rightPaddle.left && ball.top <= rightPaddle.bot && ball.bot >= rightPaddle.top && ball.xDelta === ball.speed;
+
+		if (touchingLeft || touchingRight) {
+			ball.xDelta *= -1;
+			ball.yDelta = (Math.random() * 7) - 3;
+		}
+
+		const winLeft = ball.right >= cvs.width;
+		const winRight = ball.left <= 0;
+
+		if (winLeft || winRight) {
+			if (winLeft) leftWins++;
+			else rightWins++;
+			ended = true;
+			play();
+		}
+	}, 1000 / 80);
+
+	const frame = () => {
+		if (ended) return;
+
+		reset();
+
+		ctx.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+		ctx.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+		if (poggers) {
+			pog.width = ball.width;
+			pog.height = ball.height;
+			ctx.drawImage(pog, ball.x, ball.y, ball.width, ball.height);
+		}
+		else ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+		requestAnimationFrame(frame);
+	};
+	window.onresize = () => {
+		frame();
+		rightPaddle.x = cvs.width - size;
+		rightPaddle.calcY();
+	};
+	requestAnimationFrame(frame);
+};
+
+reset();
+play();
